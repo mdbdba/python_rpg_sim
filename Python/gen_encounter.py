57e7207b427b054384a6bf0dae83ee758abe9f7b
mdbdba <mdbntl@gmail.com>
2019-05-13 16:59:08 -0500

    Add a bit more info beforehand in gen_encounter.py

--- Python/gen_encounter.py
+++ Python/gen_encounter.py
@@ -6,12 +6,18 @@ from Encounter import Encounter
 db = InvokePSQL()
 Heroes = []
 Opponents = []
-Heroes.append(PlayerCharacter(db, debug_ind=1))
-Heroes.append(PlayerCharacter(db, debug_ind=1))
-Opponents.append(Foe(db, foe_candidate='Skeleton', debug_ind=1))
-Opponents.append(Foe(db, foe_candidate='Skeleton', debug_ind=1))
-Opponents.append(Foe(db, foe_candidate='Skeleton', debug_ind=1))
-e1 = Encounter(Heroes, Opponents, debug_ind=1)
+Heroes.append(PlayerCharacter(db, debug_ind=0))
+Heroes.append(PlayerCharacter(db, debug_ind=0))
+Opponents.append(Foe(db, foe_candidate='Skeleton', debug_ind=0))
+Opponents.append(Foe(db, foe_candidate='Skeleton', debug_ind=0))
+Opponents.append(Foe(db, foe_candidate='Skeleton', debug_ind=0))
+print(f"For the Heroes:")
+for Hero in Heroes:
+    print(f"  {Hero.get_name()}")
+print(f"Against:")
+for Opponent in Opponents:
+    print(f"  {Opponent.get_name()}")
+e1 = Encounter(Heroes, Opponents, debug_ind=0)
 print(f"The winning party was: {e1.winning_list_name} in {e1.round} rounds.")
 print(f"The surviving {e1.winning_list_name} members:")
 for i in range(len(e1.winning_list)):
