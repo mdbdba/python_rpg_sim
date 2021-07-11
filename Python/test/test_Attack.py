dc57d4d641622f0a01fb17d1c6f86a41a9f034b5
mdbdba <mdbntl@gmail.com>
2021-07-10 20:15:34 -0500

    fix attack tests

--- Python/test/test_Attack.py
+++ Python/test/test_Attack.py
@@ -4,6 +4,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from Attack import Attack    # NOQA
 from Weapon import Weapon    # NOQA
+from PlayerCharacter import PlayerCharacter    # NOQA
 from InvokePSQL import InvokePSQL    # NOQA
 from Ctx import Ctx
 
@@ -11,12 +12,19 @@ def test_Attack_with_Club():
     ctx = Ctx(app_username='Testing')
     db = InvokePSQL()
     a = Weapon(db=db, ctx=ctx, name='Club')
-    b = Attack(ctx=ctx, weapon_obj=a, attack_modifier=0, damage_modifier=0)
+    c = PlayerCharacter(ctx=ctx, db=db, race_candidate='Hill dwarf',
+                        class_candidate='Fighter', ability_array_str='point_buy_even')
+    d = PlayerCharacter(ctx=ctx, db=db, race_candidate='Hill dwarf',
+                        class_candidate='Fighter', ability_array_str='point_buy_even')
+    b = Attack(ctx=ctx, weapon_obj=a, attack_modifier=0,
+                     damage_modifier=0,
+                     attack_type='Melee', attacker=c,
+                     target=d, versatile_use_2handed=False, vantage='Normal')
     assert(b.weapon_obj.name == 'Club')
     assert(b.vantage == 'Normal')
     assert(b.die_used == 4)
     assert(b.rolls_used == 1)
-    assert(b.possible_damage > 0 and b.possible_damage <= 4)
+    assert(b.possible_damage > 0 and b.possible_damage <= 6)
     assert(b.attack_value)
 
 
@@ -24,10 +32,17 @@ def test_Attack_with_Spear():
     ctx = Ctx(app_username='Testing')
     db = InvokePSQL()
     a = Weapon(db=db, ctx=ctx, name='Spear')
-    b = Attack(ctx=ctx, weapon_obj=a, attack_modifier=0, damage_modifier=0)
+    c = PlayerCharacter(ctx=ctx, db=db, race_candidate='Hill dwarf',
+                        class_candidate='Fighter', ability_array_str='point_buy_even')
+    d = PlayerCharacter(ctx=ctx, db=db, race_candidate='Hill dwarf',
+                        class_candidate='Fighter', ability_array_str='point_buy_even')
+    b = Attack(ctx=ctx, weapon_obj=a, attack_modifier=0,
+               damage_modifier=0,
+               attack_type='Ranged', attacker=c,
+               target=d, versatile_use_2handed=False, vantage='Normal')
     assert(b.weapon_obj.name == 'Spear')
     assert(b.vantage == 'Normal')
-    assert(b.die_used == 8)
+    assert(b.die_used == 6)
     assert(b.rolls_used == 1)
     assert(b.possible_damage > 0 and b.possible_damage <= 8)
     assert(b.attack_value)
